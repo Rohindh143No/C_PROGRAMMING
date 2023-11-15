37765b28eff58edf5305a78be969283dfff8d1b5
Rohindh143No <85751711+Rohindh143No@users.noreply.github.com>
2023-11-15 21:55:37 +0530

    Update urls.py

--- unit1/urls.py
+++ unit1/urls.py
@@ -4,4 +4,6 @@ urlpatterns=[
     path('unit1/',views.unit1),
     path('unit1/unit2/',views.unit2),
     path('unit1/unit2/unit3/',views.unit3),
+    path('unit1/unit2/unit3/unit4/',views.unit4),
+    path('unit1/unit2/unit3/unit4/unit5/',views.unit5),
 ]
